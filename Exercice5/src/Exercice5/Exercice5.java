c1e65914907bd852830962b54f625f4494a8ad68
hugo.dominguez <hugo.dominguez@106-08.info2>
2024-09-20 13:49:44 +0200

    ejercicio cinco

---
+++ Exercice5/src/Exercice5/Exercice5.java
@@ -0,0 +1,12 @@
+package Exercice5;
+
+public class Exercice5 {
+
+	public static void main(String[] args) {
+		System.out.println("Hugo");
+		System.out.println("Palomares del rio");
+		System.out.println();
+		System.out.println("640691865");
+	}
+
+}
